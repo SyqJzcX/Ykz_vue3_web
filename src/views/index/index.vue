8841151e66ae020b064f66a27a8fb594a8b77b5c
SyqJzcX <sqzx0524@163.com>
2025-07-09 16:07:17 +0800

    Fourth commit.

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -3,20 +3,31 @@
 </script>
 
 <template>
-  <div class="video-container" :style="{ height: containerHeight }">
+  <div class="container">
     <video ref="videoRef" autoplay muted loop playsinline class="background-video">
       <source src="@/assets/video/starrysky.mp4" type="video/mp4">
       您的浏览器不支持视频标签。
     </video>
+    <!-- <div class="content">
+
+    </div> -->
   </div>
-  <!-- <div class="content">
-  </div> -->
 </template>
 
 <style scoped>
-.video-container {
+.container {
   position: relative;
   width: 100%;
   overflow: hidden;
+  background-color: #45CB9C;
 }
+
+.background-video {
+  display: block;
+}
+
+/* .content {
+  background-color: aqua;
+  height: 70px;
+} */
 </style>
